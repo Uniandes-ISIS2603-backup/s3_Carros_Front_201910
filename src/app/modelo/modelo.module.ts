f83ec76819ea8cd6d1c93506cc4f6c32f01f9a9d
Daniel Lozano <de.lozanoe@uniandes.edu.co>
2019-04-09 19:08:01 -0500

    Modelo create html

--- src/app/modelo/modelo.module.ts
+++ src/app/modelo/modelo.module.ts
@@ -6,14 +6,17 @@ import {ModeloService} from './modelo.service';
 import { FormsModule } from '@angular/forms';
 import { ModeloDetailComponent } from './modelo-detail/modelo-detail.component';
 
+import{AppRoutingModule} from '../app-routing/app-routing.module';
+import {ModeloCreateComponent} from './modelo-create/modelo-create.component';
 
 @NgModule({
   imports: [
     CommonModule, 
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  declarations: [ModeloListComponent, ModeloDetailComponent],
-  exports: [ModeloListComponent],
+  declarations: [ModeloListComponent, ModeloDetailComponent, ModeloCreateComponent],
+  exports: [ModeloListComponent, ModeloCreateComponent],
   providers: [ModeloService]
 })
 export class ModeloModule { }
--- src/app/punto-venta/punto-venta-detail.ts
+++ src/app/punto-venta/punto-venta-detail.ts
@@ -1,14 +1,22 @@
 import{PuntoVenta} from './punto-venta';
+import{QuejasReclamos} from '../quejasReclamos/quejas-reclamos';
+import {Marca} from'../marca/marca';
+import {Registros} from '../registros/registros';
+
+
 
 export class PuntoVentaDetail extends PuntoVenta
 {
+
   clientes: String;
 
   Empleado: String; 
 
-  RegistroCompra: String; 
+  QuejasReclamos: QuejasReclamos[];
 
-  CompraVenta: String; 
+  RegistroCompra: Registros[]; 
+
+  CompraVenta: Marca[]; 
 
   Automovil: String; 
 }
