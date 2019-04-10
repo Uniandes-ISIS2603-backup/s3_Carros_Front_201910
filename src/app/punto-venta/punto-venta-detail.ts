5070bd5ded2c7e66e0a407335565670ec00b024c
Daniel Lozano <de.lozanoe@uniandes.edu.co>
2019-04-09 20:50:56 -0500

    Arreglado llmado create

--- src/app/punto-venta/punto-venta-detail.ts
+++ src/app/punto-venta/punto-venta-detail.ts
@@ -2,7 +2,7 @@ import{PuntoVenta} from './punto-venta';
 import{QuejasReclamos} from '../quejasReclamos/quejas-reclamos';
 import {Marca} from'../marca/marca';
 import {Registros} from '../registros/registros';
-import {Empleado} from '../empleado/empleado';
+
 
 
 
@@ -11,13 +11,13 @@ export class PuntoVentaDetail extends PuntoVenta
 
   clientes: String;
 
-  Empleado: Empleado[]; 
+  Empleado: String[]; 
 
   QuejasReclamos: QuejasReclamos[];
 
   RegistroCompra: Registros[]; 
 
-  Marcas: Marca[]; 
+  marcas: Marca[]; 
 
   Automovil: String; 
 }
